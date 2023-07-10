031173688ed1b0cd0d3a417a4930f66b4c7a057f
Prabin Acharya <acharyaprabin101@gmail.com>
2023-07-10 09:16:03 +0545

    image upload to gcs works

---
+++ pages/api/upload.ts
@@ -0,0 +1,79 @@
+import { Storage } from "@google-cloud/storage";
+import formidable from "formidable";
+import fs from "fs";
+import { NextApiRequest, NextApiResponse, PageConfig } from "next";
+
+const storage = new Storage({
+  projectId: "gcp-mongo-hackathon", // Replace with your project ID
+  keyFilename: "gcp-mongo-hackathon-key.json", // Replace with the path to your service account key file
+});
+
+const bucket = storage.bucket("gcp-mongo-hackathon-docker-cloud-storage"); // Replace with your bucket name
+
+const formidableConfig = {
+  keepExtensions: true,
+  maxFileSize: 10_000_000,
+  maxFieldsSize: 10_000_000,
+  maxFields: 7,
+  allowEmptyFiles: false,
+  multiples: false,
+};
+
+function formidablePromise(
+  req: NextApiRequest,
+  opts?: Parameters<typeof formidable>[0]
+): Promise<{ fields: formidable.Fields; files: formidable.Files }> {
+  return new Promise((accept, reject) => {
+    const form = formidable(opts);
+
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return reject(err);
+      }
+      return accept({ fields, files });
+    });
+  });
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const { fields, files } = await formidablePromise(req, formidableConfig);
+
+    const file = files.file[0];
+
+    console.log(file.originalFilename, file.filepath, "file");
+
+    // Create a new blob in the bucket and upload the file data
+    const fileName = file.originalFilename.split(".")[0] + file.newFilename;
+    const blob = bucket.file(fileName);
+    const blobStream = blob.createWriteStream();
+
+    blobStream.on("error", (err) => {
+      console.error(err);
+      return res.status(500).json({ error: "Internal Server Error" });
+    });
+
+    blobStream.on("finish", () => {
+      const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+      res.status(200).send({ url: publicUrl });
+    });
+
+    // blobStream.end(file.data);
+    // Create a read stream from the temporary file and pipe it to the blobStream
+    fs.createReadStream(file.filepath).pipe(blobStream);
+
+    return res.status(200).json({ fields, files, msg: "success" });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+export const config: PageConfig = {
+  api: {
+    bodyParser: false,
+  },
+};
---
+++ pages/upload.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+import { useState } from "react";
+import { useDropzone } from "react-dropzone";
+
+export default function UploadImage() {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [imageFile, setImageFile] = useState<File | null>(null);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/*",
+    onDrop: (acceptedFiles) => {
+      setSelectedImage(URL.createObjectURL(acceptedFiles[0]));
+      setImageFile(acceptedFiles[0]);
+    },
+  });
+
+  const uploadImage = async () => {
+    if (!imageFile) return;
+
+    const formData = new FormData();
+    formData.append("file", imageFile);
+    formData.append("x-file-name", imageFile.name);
+
+    const response = await fetch("/api/upload", {
+      method: "POST",
+      body: formData,
+      headers: {
+        "x-file-name": imageFile.name,
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    if (response.ok) {
+      console.log("Image uploaded successfully");
+    } else {
+      console.log("Image upload failed");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Upload your image</h1>
+      <div>
+        <div {...getRootProps()}>
+          <input {...getInputProps()} />
+          <p>Drag and drop some files here, or click to select files</p>
+        </div>
+        {selectedImage && (
+          <>
+            <Image
+              src={selectedImage}
+              alt="Selected"
+              width={400}
+              height={300}
+            />
+            <button onClick={uploadImage}>Upload</button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
